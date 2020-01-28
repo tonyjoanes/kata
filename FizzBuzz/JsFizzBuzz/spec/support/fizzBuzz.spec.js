761ead11116303394906c91a600420503c6045bc
Tony Joanes <tonyjoanes@outlook.com>
2020-01-28 20:18:09 +0000

    prepare fizz buzz js kata

---
+++ FizzBuzz/JsFizzBuzz/spec/support/fizzBuzz.spec.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    describe('When working with the fizz buzz program', () => {
+
+        it('should test 1 + 1', () => {
+            expect(2).toBe(2);
+        });
+    });
+})();
